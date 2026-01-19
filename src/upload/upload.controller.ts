9aaa565254169cf77bf9f7de0615178f0af59140
Pedro <pedromrap@gmail.com>
2026-01-19 11:19:57 -0300

    feat: implement upload module with file handling, validation, and storage

---
+++ src/upload/upload.controller.ts
@@ -0,0 +1,24 @@
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+import { storage, limits, fileFilter } from './upload.config';
+import { UploadService } from './upload.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.auth.guard';
+
+@Controller('upload')
+export class UploadController {
+  constructor(private readonly uploadService: UploadService) {}
+
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  @UseInterceptors(FileInterceptor('file', { storage, limits, fileFilter }))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return this.uploadService.handleUpload(file);
+  }
+}
